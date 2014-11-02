d8ddb187188feceb762f4aa3b7052bc4d84b7afa
Jackson Henry <jackson.henry3@gmail.com>
2014-11-02 16:16:56 -0500

    added vector field

--- js/justGravity.js
+++ js/justGravity.js
@@ -6,7 +6,7 @@ var data   = [],
 	G      = -6.67*Math.pow(10,-11), //m^3/(kg s^2)
 	w      = document.body.scrollWidth,
 	h      = document.body.scrollHeight,
-    A      = Math.pow(10,35), //Pauli scaling factor
+    A      = 1, //Pauli scaling factor
 	mpp    = 2*scale/h, // meters per pixel
 	canvas = d3.select("#canvas"), 
 	svg    = canvas.append("svg:svg");
@@ -21,6 +21,7 @@ h_scale = d3.scale.linear()
 
 svg.attr("width", w)
    .attr("height", h)
+   .attr('id','space')
    .style("background-color",'#222')
    .style("pointer-events", "all")
    .on("click",add_data) 
@@ -42,11 +43,11 @@ function add_data(){
 		.attr("cx",function(d){return(w_scale.invert(d.r.x))})
 		.attr("cy",function(d){return(h_scale.invert(d.r.y))}) 
 		.attr("r",10)
-		.attr("fill",function(){return('#'+Math.floor(Math.random()*16777215).toString(16));});
+		.attr("fill","#7fffd4");
 }
 
 update_position = function (){
-	var h = 100.;
+	var h = 1000.;
 	for (var i = data.length - 1; i >= 0; i--){
 		d   = data[i]
 		k1 = gravityAcceleration(d.r,i)
@@ -60,6 +61,7 @@ update_position = function (){
 	svg.selectAll("circle")
 		.attr('cx',function(d){return(w_scale.invert(d.r.x))})
 		.attr('cy',function(d){return(h_scale.invert(d.r.y))})
+    updateField()
     window.requestAnimationFrame(update_position);
 };
 
@@ -70,13 +72,19 @@ function gravityAcceleration(r,index)
 			if (i != index)
 			{
 				var r2 = data[i].r,
-				    dr = r.subtract(r2),
-				    m2 = data[i].m,
+				    dr = r.subtract(r2);
+                if (dr.magnitude()<scale/2 && dr.magnitude()>(mpp*20))
+                {
+
+				var    m2 = data[i].m,
 				    av = dr.norm().scale(G*m2/(Math.pow(dr.magnitude(),2)));
                 
 				a = a.add(av)
+                                    
+                }
 			};
 		};
+        return(a)
 		return(pauliAcceleration(a,r,index));
 	};
 
@@ -94,5 +102,41 @@ function pauliAcceleration(a,r,index)
 		};
 		return(a);
 	};
+
+
+n   = 25;
+vectorData = []
+for (var i = n - 1; i >= 0; i--)
+{
+    for (var j = n- 1; j >= 0; j--)
+    {
+        origin = new vector([w_scale((i/n+1/(2*n))*w),h_scale((j/n+1/(2*n))*h)])
+        v = new vector([0,0])
+        vectorData.push({origin:origin,vec:v})
+    };
+};
+
+svg.selectAll("line")
+    .data(vectorData)
+    .enter()
+    .append("line")
+    .attr("x1",function(d){return(w_scale.invert(d.origin.x))})
+    .attr("y1",function(d){return(h_scale.invert(d.origin.y))})
+    .attr("x2",function(d){return(w_scale.invert(d.origin.x+d.vec.x))})
+    .attr("y2",function(d){return(h_scale.invert(d.origin.y+d.vec.y))})
+    .attr("stroke",'teal');
+
+function updateField()
+{
+    for (var i = vectorData.length - 1; i >= 0; i--)
+    {
+        vectorData[i].vec = gravityAcceleration(vectorData[i].origin,null)
+
+    };
+    
+    svg.selectAll("line")
+        .attr("x2",function(d){return(w_scale.invert(d.origin.x+d.vec.x*30000000))})
+        .attr("y2",function(d){return(h_scale.invert(d.origin.y+d.vec.y*30000000))})
+};
 
 update_position()
